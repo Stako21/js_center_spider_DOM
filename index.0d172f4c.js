6711acbc5ddee61120016798096f4f092f39f679
Stanislav Kohut <stako211@gmail.com>
2024-10-21 22:04:47 +0300

    make build

---
+++ index.0d172f4c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=e.clientWidth/2,c=e.clientHeight/2,l=document.querySelector(".spider"),i=l.clientWidth/2,n=l.clientHeight/2;l.style.top="".concat(t-i,"px"),l.style.left="".concat(c-n,"px");
+//# sourceMappingURL=index.0d172f4c.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const centerWallWidth = wall.clientWidth / 2;
+const centerWallHeight = wall.clientHeight / 2;
+const spider = document.querySelector(".spider");
+const centerSpiderWidth = spider.clientWidth / 2;
+const centerSpiderHeight = spider.clientHeight / 2;
+spider.style.top = `${centerWallWidth - centerSpiderWidth}px`;
+spider.style.left = `${centerWallHeight - centerSpiderHeight}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
